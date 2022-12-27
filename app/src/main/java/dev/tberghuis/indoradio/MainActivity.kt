0445e8bdcc113e9f8c493bd25a4fe124bc3f0a4e
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-12-28 07:13:59 +1100

    .

--- app/src/main/java/dev/tberghuis/indoradio/MainActivity.kt
+++ app/src/main/java/dev/tberghuis/indoradio/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dev.tberghuis.indoradio.home.HomeScreen
 import dev.tberghuis.indoradio.ui.theme.IndoRadioTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,24 +18,10 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       IndoRadioTheme {
-        // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+HomeScreen()
         }
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-  IndoRadioTheme {
-    Greeting("Android")
   }
 }
---
+++ app/src/main/java/dev/tberghuis/indoradio/home/HomeScreen.kt
@@ -0,0 +1,13 @@
+package dev.tberghuis.indoradio.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+  Text("hello home screen")
+}
+
+class FakeVm {
+//  val player
+}
